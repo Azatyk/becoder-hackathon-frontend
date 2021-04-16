60199c8ea77ca7dc5b5bfa04bc81ef17b82ff1e0
Donchidzee <47913017+Donchidzee@users.noreply.github.com>
2021-04-17 03:30:05 +0600

    feat: add email page opening

--- src/views/intro/pine-intro.vue
+++ src/views/intro/pine-intro.vue
@@ -1,30 +1,52 @@
 <template>
-  <div class="intro">
-    <div
-      class="intro-loading"
-      :class="{
-        'intro-loading-open': isLoadingOpen,
-      }"
-    >
-      <div class="intro-loading-container">
-        <img src="@/assets/images/pine-logo.svg" alt="pine_logo" />
-        <div class="intro-loading-text">Pine</div>
+  <div>
+    <transition name="welcome" v-if="this.welcome">
+      <div class="intro">
+        <div
+          class="intro-loading"
+          :class="{
+            'intro-loading-open': isLoadingOpen,
+          }"
+        >
+          <div class="intro-loading-container">
+            <img src="@/assets/images/pine-logo.svg" alt="pine_logo" />
+            <div class="intro-loading-text">Pine</div>
+          </div>
+        </div>
+        <div class="intro-image">
+          <img
+            src="@/assets/images/welcome-image.svg"
+            alt="welcome_image"
+            class="intro-image-content"
+          />
+        </div>
+        <h1 class="intro-title">Добро пожаловать в Pine!</h1>
+        <p class="intro-text">
+          Узнай историю мест и времени своих фотографией,<br />
+          отредактируй и узнай какие объекты расположены с помощью<br />
+          Pine. Но для начала нам понадобиться твоя электронная почта:
+        </p>
+        <pine-button class="intro-button" @click="welcome = false"
+          >Начать</pine-button
+        >
       </div>
+    </transition>
+    <div class="intro" v-else>
+      <div class="intro-image">
+        <img
+          src="@/assets/images/pine-email.svg"
+          alt="welcome_image"
+          class="intro-image-content"
+        />
+      </div>
+      <h1 class="intro-title intro-title-email">
+        Для начала нам понадобиться твоя электронная почта
+      </h1>
+      <input placeholder="Введите адрес почты" class="intro-input" />
+      <pine-button class="intro-button" @click="welcome = false"
+        >Далее</pine-button
+      >
     </div>
-    <div class="intro-image">
-      <img
-        src="@/assets/images/welcome-image.svg"
-        alt="welcome_image"
-        class="intro-image-content"
-      />
-    </div>
-    <h1 class="intro-title">Добро пожаловать в Pine!</h1>
-    <p class="intro-text">
-      Узнай историю мест и времени своих фотографией,<br />
-      отредактируй и узнай какие объекты расположены с помощью<br />
-      Pine. Но для начала нам понадобиться твоя электронная почта:
-    </p>
-    <pine-button class="intro-button">Начать</pine-button>
   </div>
 </template>
 
@@ -38,6 +60,7 @@ export default {
   data() {
     return {
       isLoadingOpen: true,
+      welcome: true,
     };
   },
   mounted() {
@@ -106,6 +129,11 @@ export default {
   &-title {
     margin-top: 30px;
     color: $main-dark;
+
+    &-email {
+      width: 650px;
+      text-align: center;
+    }
   }
 
   &-text {
@@ -117,8 +145,53 @@ export default {
     opacity: 0.8;
   }
 
+  &-input {
+    margin-top: 30px;
+    border: 2px solid $primary;
+    padding: 10px 15px;
+    border-radius: 10px;
+    width: 300px;
+    outline: none;
+    box-sizing: border-box;
+    transition: 200ms ease-in-out;
+  }
+
+  &-input:focus {
+    padding-left: 18px;
+  }
+
   &-button {
     margin-top: 40px;
   }
+}
+
+.welcome-enter-active {
+  transition: opacity 1s ease-in-out;
+  animation: longFadeIn 2s ease-in-out;
+  will-change: opacity;
+}
+
+@keyframes longFadeIn {
+  0% {
+    opacity: 0;
+  }
+
+  70% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+
+.welcome-leave-active {
+  transition: opacity 1s ease-in-out;
+  will-change: opacity;
+}
+
+.welcome-enter,
+.welcome-leave-to {
+  opacity: 0;
 }
 </style>
