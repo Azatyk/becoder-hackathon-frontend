05e37e64f1bf544c8522e2034438f8a12b8fb6a4
Donchidzee <47913017+Donchidzee@users.noreply.github.com>
2021-04-17 15:10:21 +0600

    feat: add ant design components

--- src/views/input/pine-input.vue
+++ src/views/input/pine-input.vue
@@ -1,9 +1,140 @@
 <template>
-  <div class="input-page">input</div>
+  <div class="input-page">
+    <a-upload-dragger
+      name="file"
+      :multiple="true"
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      @change="handleChange"
+      class="input-page-input"
+    >
+      <p class="ant-upload-drag-icon">
+        <a-icon type="inbox" />
+      </p>
+      <p class="ant-upload-text">Нажми или перетащи файлы в область загрузки</p>
+      <p class="ant-upload-hint">
+        Support for a single or bulk upload. Strictly prohibit from uploading
+        company data or other band files
+      </p>
+    </a-upload-dragger>
+    <div class="input-page-container">
+      <a-upload
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        list-type="picture-card"
+        :file-list="fileList"
+        @preview="handlePreview"
+        @change="handleChan"
+      ></a-upload>
+      <a-modal :visible="previewVisible" :footer="null" @cancel="handleCancel">
+        <img alt="example" style="width: 100%" :src="previewImage" />
+      </a-modal>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+}
+export default {
+  data() {
+    return {
+      previewVisible: false,
+      previewImage: "",
+      fileList: [
+        {
+          uid: "-1",
+          name: "image.png",
+          status: "done",
+          url:
+            "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        },
+        {
+          uid: "-2",
+          name: "image.png",
+          status: "done",
+          url:
+            "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        },
+        {
+          uid: "-3",
+          name: "image.png",
+          status: "done",
+          url:
+            "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        },
+        {
+          uid: "-4",
+          name: "image.png",
+          status: "done",
+          url:
+            "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        },
+        {
+          uid: "-5",
+          name: "image.png",
+          status: "error",
+        },
+        {
+          uid: "-2",
+          name: "image.png",
+          status: "done",
+          url:
+            "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        },
+        {
+          uid: "-3",
+          name: "image.png",
+          status: "done",
+          url:
+            "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        },
+        {
+          uid: "-4",
+          name: "image.png",
+          status: "done",
+          url:
+            "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        },
+        {
+          uid: "-5",
+          name: "image.png",
+          status: "error",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleChange(info) {
+      const status = info.file.status;
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (status === "done") {
+        this.$message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === "error") {
+        this.$message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+    handleCancel() {
+      this.previewVisible = false;
+    },
+    async handlePreview(file) {
+      if (!file.url && !file.preview) {
+        file.preview = await getBase64(file.originFileObj);
+      }
+      this.previewImage = file.url || file.preview;
+      this.previewVisible = true;
+    },
+    handleChan({ fileList }) {
+      this.fileList = fileList;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -14,5 +145,16 @@ export default {};
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  &-input {
+    height: 300px;
+    width: 700px;
+  }
+
+  &-container {
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-around;
+  }
 }
 </style>
