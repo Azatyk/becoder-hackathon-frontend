616c2329fe91bfdbf556e0434fd4d4b4d93734f3
Donchidzee <47913017+Donchidzee@users.noreply.github.com>
2021-04-17 11:23:16 +0600

    feat: create input page

---
+++ src/views/input/pine-input.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="input-page">input</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.input-page {
+  height: 100vh;
+  box-sizing: border-box;
+  padding: 100px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
